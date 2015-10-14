59a32bcbaab8fd9c1415860a713b2f5239a382ac
Fabien Rogeret <fabien.rogeret@gmail.com>
2015-10-13 23:40:50 -0400

    Audio source

--- src/components/audio/audio.js
+++ src/components/audio/audio.js
@@ -1,3 +1,61 @@
+class Audio {
+    constructor() {
+        navigator.getUserMedia = (
+            navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia
+        );
+
+        if (!navigator.getUserMedia) {
+            throw new Error("getUserMedia not supported on your browser!")
+        }
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            navigator.getUserMedia ({audio: true},
+                // Success callback
+                (stream) => {
+                    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                    this.analyser = audioCtx.createAnalyser();
+                    this.analyser.fftSize = 256;
+                    let distortion = audioCtx.createWaveShaper();
+
+                    let source = audioCtx.createMediaStreamSource(stream);
+
+                    source.connect(this.analyser);
+                    this.analyser.connect(distortion);
+                    resolve();
+                },
+                (err) => {
+                    reject('The following gUM error occured: ' + err);
+                }
+            );
+        });
+    }
+
+    set fftSize(size) {
+        this.analyser.fftSize = size;
+    }
+
+    get data() {
+        let bufferLength = this.analyser.fftSize,
+            dataArray = new Uint8Array(bufferLength);
+
+        this.analyser.getByteTimeDomainData(dataArray);
+
+        return dataArray;
+    }
+}
+
 export function init() {
-    console.log('audio initialized...');
+    var audio = new Audio();
+
+    audio.init().then(() => {
+        setInterval(() => {
+            console.log(audio.data);
+        }, 500);
+
+    });
 }
